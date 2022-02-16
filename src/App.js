947c9770ccff1c6a6f762dbf886e19079c8d6d4c
koutaro20 <koutarodocomo@outlook.jp>
2022-02-16 15:55:16 +0900

    todo add

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React, { useState } from 'react'
 import "./index.css"
 
 export const App = () => {
-    const [incompleteTodos, setIncompleteTodos] = useState(['ああああ', 'いいいい']);
+    const [todoText, setTodoText] = useState('');
+    const [incompleteTodos, setIncompleteTodos] = useState(['あああ', 'いいい']);
     const [completeTodos, setCompleteTodos] = useState(['うううう'])
+
+    const onChangeTodoText = (event) => setTodoText(event.target.value);
+    const onClickAdd = () => {
+        if (todoText==="") return
+        const newTodos = [...incompleteTodos, todoText]
+        setIncompleteTodos(newTodos)
+        setTodoText('')
+    }
     return (
         <>
             <div className="input-area">
-                <input placeholder='TODOを入力'></input>
-                <button>追加</button>
+                <input placeholder='TODOを入力' value={todoText} onChange={onChangeTodoText}></input>
+                <button onClick = { onClickAdd }>追加</button>
             </div>
             <div className="incomplete-area">
                 <p className = "title">未完了のTODO</p>
