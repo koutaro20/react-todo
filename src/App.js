add0a91c2bdfebdc05384c296a39dae1e2429d59
koutaro20 <koutarodocomo@outlook.jp>
2022-02-16 16:14:16 +0900

    todo remove

--- src/App.js
+++ src/App.js
@@ -7,11 +7,18 @@ export const App = () => {
     const [completeTodos, setCompleteTodos] = useState(['うううう'])
 
     const onChangeTodoText = (event) => setTodoText(event.target.value);
+
     const onClickAdd = () => {
         if (todoText==="") return
         const newTodos = [...incompleteTodos, todoText]
         setIncompleteTodos(newTodos)
         setTodoText('')
+    }
+
+    const onClickDelete = (index) => {
+        const newTodos = [...incompleteTodos]
+        newTodos.splice(index, 1)
+        setIncompleteTodos(newTodos)
     }
     return (
         <>
@@ -22,12 +29,12 @@ export const App = () => {
             <div className="incomplete-area">
                 <p className = "title">未完了のTODO</p>
                 <ul>
-                    {incompleteTodos.map((todo) => {
+                    {incompleteTodos.map((todo, index) => {
                         return (
                             <div key={todo} className='list-row'>
                                 <li>{todo}</li>
                                 <button>完了</button>
-                                <button>削除</button>
+                                <button onClick = {() => onClickDelete(index)}>削除</button>
                             </div>
                         )
                     })}
