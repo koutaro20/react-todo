08afdc57277aff1d9714fbfef0af2e004caf6de1
koutaro20 <koutarodocomo@outlook.jp>
2022-02-17 15:23:36 +0900

    last commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react'
+import { InputTodo } from './components/inputTodo'
+import { IncompleteTodos } from './components/incompleteTodo'
+import { CompleteTodos } from './components/completeTodo'
 import "./index.css"
 
 export const App = () => {
@@ -41,37 +44,12 @@ export const App = () => {
 
     return (
         <>
-            <div className="input-area">
-                <input placeholder='TODOを入力' value={todoText} onChange={onChangeTodoText}></input>
-                <button onClick = { onClickAdd }>追加</button>
-            </div>
-            <div className="incomplete-area">
-                <p className = "title">未完了のTODO</p>
-                <ul>
-                    {incompleteTodos.map((todo, index) => {
-                        return (
-                            <div key={todo} className='list-row'>
-                                <li>{todo}</li>
-                                <button onClick = {() => onClickComplete(index)}>完了</button>
-                                <button onClick = {() => onClickDelete(index)}>削除</button>
-                            </div>
-                        )
-                    })}
-                </ul>
-            </div>
-            <div className="complete-area">
-                <p className = "title">完了のTODO</p>
-                <ul>
-                    {completeTodos.map((todo, index) => {
-                        return(
-                            <div key={todo} className='list-row'>
-                                <li>{todo}</li>
-                                <button onClick = {() => onClickBack(index)}>戻す</button>
-                            </div>
-                        )
-                    })}
-                </ul>
-            </div>
+            <InputTodo todoText={todoText} onChange={onChangeTodoText} onClick={onClickAdd} disabled={incompleteTodos.length >= 5}/>
+            {incompleteTodos.length >= 5 && (
+                <p>登録できるtodoは5個までだよ～消化しろ～</p>
+            )}
+            <IncompleteTodos todos={incompleteTodos} onClickComplete={onClickComplete} onClickDelete={onClickDelete} />
+            <CompleteTodos todos = {completeTodos} onClickBack={onClickBack} />
         </>
     )
 }
